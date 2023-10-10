5209abcb31cf2252287ecec9c8672257d10f1efc
Test <test@test.pl>
2023-10-10 12:37:55 +0200

    -App end

--- Pierwszy projekt-Helper/FirstTaskWPF/MainWindow.xaml.cs
+++ Pierwszy projekt-Helper/FirstTaskWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,40 @@ namespace FirstTaskWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+   
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+            if(string.IsNullOrWhiteSpace(textBoxUserName.Text) || textBoxUserName.Text == "")
+            //if (textBoxUserName.Text == "")
+            {
+                textBlockResult.Text = "Nie podano imienia ";
+            }
+            
+            if (textBoxAge.Text == "")
+            {
+                textBlockResult.Text = "Nie podano wieku";
+            }
+
+            if ((textBoxAge.Text == "" && textBoxUserName.Text == "") || (string.IsNullOrWhiteSpace(textBoxUserName.Text) && textBoxAge.Text == ""))
+            {
+                textBlockResult.Text = "Nie podano wieku i imienia";
+            }
+
+            if (!int.TryParse(textBoxAge.Text, out number) && textBoxAge.Text != "")
+            {
+                textBlockResult.Text = "Wiek podaj w liczbach";
+            }
+
+            if (int.TryParse(textBoxAge.Text, out number) && number >= 18 && textBoxUserName.Text != "" && !string.IsNullOrWhiteSpace(textBoxUserName.Text))
+            {
+              textBlockResult.Text = "Osoba " + textBoxUserName.Text + " osoba pełnoletnia";
+            }
+            if (int.TryParse(textBoxAge.Text, out number) && number < 18 && textBoxUserName.Text != "" && !string.IsNullOrWhiteSpace(textBoxUserName.Text))
+                textBlockResult.Text = "Osoba " + textBoxUserName.Text + " nie jest pełnoletnia";
+            
         }
     }
 }

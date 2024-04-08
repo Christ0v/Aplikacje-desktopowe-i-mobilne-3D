20bb897c7a502911397135332125b2e5acd2e5d9
Test <test@test.pl>
2024-04-08 14:42:32 +0200

    -dodano obsługę latarki(nie działa)

---
+++ Pierwszy projekt-Helper/EssentialsApp/EssentialsApp/Flashlight.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace EssentialsApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Flashlight : ContentPage
+    {
+        public Flashlight()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnOn_Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Xamarin.Essentials.Flashlight.TurnOnAsync();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        private void TurnOf_Button_Clicked(object sender, EventArgs e)
+        {
+            Xamarin.Essentials.Flashlight.TurnOffAsync();
+        }
+    }
+}
